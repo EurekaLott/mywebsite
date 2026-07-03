a451ab86c61f233491bd65405c501c2c658aaf89
EurekaLott <trongcuong.org@gmail.com>
2026-07-03 11:18:11 +0700

    Update bruce-banner.js

--- bruce-banner.js
+++ bruce-banner.js
@@ -1,22 +1,13 @@
 console.log("🥋 Bruce Banner Loaded");
-// ===============================
-// Bruce Banner - Round 3
-// ===============================
 
-// Tạo banner nếu chưa có
-const header = document.querySelector(".header");
+const banner = document.getElementById("bruce-banner");
 
-if (header && !document.getElementById("bruce-banner")) {
-
-    const banner = document.createElement("div");
-    banner.id = "bruce-banner";
+if (!banner) {
+    console.error("❌ #bruce-banner not found");
+} else {
+    console.log("🥋 Bruce Banner Ready");
 
     banner.innerHTML = `
         <canvas id="bruceCanvas"></canvas>
     `;
-
-    // chèn ngay dưới Header
-    header.insertAdjacentElement("afterend", banner);
-
-    console.log("🥋 Bruce Banner Created");
 }
